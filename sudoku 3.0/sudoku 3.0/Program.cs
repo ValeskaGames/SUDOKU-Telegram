ec8c9354a9a00b49f0b5344817b9f9299262df03
Basedfloppa <113017705+Basedfloppa@users.noreply.github.com>
2022-10-28 04:35:36 +0300

    fixed compatibility between two classes
    gameplay loop is working, at least you can "play" sudoku now

--- sudoku 3.0/sudoku 3.0/Program.cs
+++ sudoku 3.0/sudoku 3.0/Program.cs
@@ -6,9 +6,18 @@ using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
 using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
 
 namespace BotCode
 {
+    enum State
+    {
+        Menu,
+        Game,
+        Conclusion,
+        Log,
+        Statistics
+    }
     enum Verticals
     {
         A = 1,
@@ -23,48 +32,106 @@ namespace BotCode
     }
     class Program
     {
+        public static State state = State.Menu;
+        public static Base_Game game = new Base_Game();
         static ITelegramBotClient bot = new TelegramBotClient(""); // token
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            // actions on message recive
-            Console.WriteLine($"{update.Message.From.Username}:{update.Message.Text}");
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            
+            if (update.Message != null && update.Message.Text != null)
             {
                 var message = update.Message;
-                if (message.Text.ToLower() == "/start")
+                Console.WriteLine(message.Chat.Id + ":" + message.Text);
+                switch (state)
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать!");
-                    return;
+                    case State.Menu:
+                        {
+                            if (message.Text.ToLower() == "/start")
+                            {
+                                await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать!\nДля начала игры напипише /start_game\n/start_game_n для выбора сложности, чем больше n, тем больше клеток будет заполнено.");
+                            }
+                            if (message.Text.ToLower() == "/start_game")
+                            {
+                                state = State.Game;
+                                await botClient.SendTextMessageAsync(message.Chat, game.Render());
+                            }
+                            if (message.Text.ToLower().Contains("/start_game_n"))
+                            {
+                                break;
+                            }
+                            if (message.Text.ToLower().Contains("/start_game_"))
+                            {
+                                int n = message.Text.ToLower().LastIndexOf("_");
+                                n = Convert.ToInt32(message.Text.Substring(n + 1));
+                                if (n > 81) {await botClient.SendTextMessageAsync(message.Chat, "В сетке даже столько ячеек нет..."); break; }
+                                if (n >= 40 && n <= 81) { await botClient.SendTextMessageAsync(message.Chat, "Не хорошо задавать такую низкую сложность :)"); }
+                                if (n >= 0) { game.EnforceDifficulty(n); state = State.Game; await botClient.SendTextMessageAsync(message.Chat, game.Render()); }
+                            }
+                            if (message.Text == "state")
+                            {
+                                await botClient.SendTextMessageAsync(message.Chat, Convert.ToString(state));
+                            }
+                            break;
+                        }
+                    case State.Game:
+                        {
+                            if (message.Text == "state")
+                            {
+                                await botClient.SendTextMessageAsync(message.Chat, Convert.ToString(state));
+                            }
+                            if (message.Text != null)
+                            {
+                                int i, j, arg;
+                                string s = " ";
+                                (i, j, arg) = game.Input(message.Text);
+                                if (game.IsValid(i, j, arg) == true) { game.Write(i, j, arg); s = game.Render(); }
+                                else if (game.IsValid(i, j, arg) == false) { s = "invalid input"; }
+                                else if (game.Prediction() == 0) { Console.Write("lose"); state = State.Conclusion; }
+                                else if (game.Prediction() == 1) { Console.Write("Win"); state = State.Conclusion; }
+                                await botClient.SendTextMessageAsync(message.Chat, s);
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("state exception");
+                            break;
+                        }
                 }
-                await botClient.SendTextMessageAsync(message.Chat, $"{message.Text}");
-                Base_Game buba = new Base_Game(10);
-                botClient.SendTextMessageAsync(message.Chat, buba.Render());
-                //bot.DeleteMessageAsync(message.Chat.Id, message.MessageId, default);
-                //bot.EditMessageTextAsync(message.Chat.Id , message.MessageId, "_-_-_-");
+
             }
-        }
+        } // actions on message recive
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
-            // exeption handling
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
-        }
+            Console.WriteLine("base exeption");
+        }// exeption handling
         static void Main(string[] args) // console 
         {
             Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
-
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
             var receiverOptions = new ReceiverOptions
             {
                 AllowedUpdates = { }, // receive all update types
             };
-            bot.StartReceiving(
+            bot.StartReceiving
+            (
                 HandleUpdateAsync,
                 HandleErrorAsync,
                 receiverOptions,
                 cancellationToken
             );
-            Console.ReadLine();
+            for(; ; )
+            {
+                int cid = 1377091495;
+                string s = Console.ReadLine();
+                string c = s.Remove(s.IndexOf(" ")).Trim();
+                if(c == "саня") { cid = 1583017092; }
+                else if(c == "я") { cid = 1377091495; }
+                else if(c == "владик") { cid = 904653772; }
+                s = s.Substring(s.IndexOf(" ")).Trim();
+                bot.SendTextMessageAsync(Convert.ToInt32(cid),s);
+            }
         }
     }
     public class Base_Game
@@ -73,10 +140,11 @@ namespace BotCode
         string [,] Region_Array = new string[3, 3]; // box  array +
         string[] Vertical_Array = new string[9]; // collum array +
         string[] Horizontal_Array = new string[9]; // row array +
-        int[,] Elements = new int[9, 9]; // basic array +
+        int[,] Elements = new int[9, 9] ; // basic array +
         string[,] Elements_Prediction = new string[9, 9]; // possible numbers for each cell + win/lose conditions +
         public Base_Game()
         {
+            Difficulty = 0;
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
@@ -89,48 +157,41 @@ namespace BotCode
                 Horizontal_Array[i] = "";
                 Vertical_Array[i] = "";
             }
-            if (Difficulty > 0) { Enforce_Difficulty(Difficulty); }
+            if (Difficulty > 0) { EnforceDifficulty(Difficulty); }
             Prediction();
             Render();
         } // programm start + nullyfing of the arrays +
-        public Base_Game(int d)
+        public (int i, int j, int arg) Input(string a)
         {
-            Difficulty = d;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Region_Array[i, j] = "";
-                }
-            }
-            for (int i = 0; i < 9; i++)
-            {
-                Horizontal_Array[i] = "";
-                Vertical_Array[i] = "";
-            }
-            if (Difficulty > 0) { Enforce_Difficulty(Difficulty); }
-            Prediction();
-            Render();
-        } // programm start + nullyfing of the arrays + custom difficulty (more = easier)
+            a.Trim();
+            int _i = Convert.ToInt32(a.Remove(a.IndexOf(" "))) - 1;
+            a = a.Substring(2);
+            a.TrimStart();
+            int _j = Convert.ToInt32(a.Remove(a.IndexOf(" "))) - 1;
+            a = a.Substring(2);
+            a.TrimStart();
+            int _arg = Convert.ToInt32(a);
+            return (_i, _j, _arg);
+        }
         public void Write(int i, int j, int arg)
         {
-            if (Is_Valid(i, j, arg) == true && Elements[i, j] == null)
+            if (IsValid(i, j, arg) == true && Elements[i, j] == 0)
             {
                 Elements[i, j] = arg; 
                 Region_Array[i / 3, j / 3] += arg; Horizontal_Array[i] += arg; Vertical_Array[j] += arg;
             }
             else { Error(); }
-            Prediction();
             Render();
-        } // setting the value in box +
-        public bool Is_Valid(int i, int j, int arg)
+        } // setting the value in cell +
+        public bool IsValid(int i, int j, int arg)
         {
             if (Horizontal_Array[i].Contains(arg.ToString())) { return false; }
             if (Vertical_Array[j].Contains(arg.ToString())) { return false; }
             if (Region_Array[i / 3, j / 3].Contains(arg.ToString())) { return false; }
+            if (Elements[i,j] != 0) { return false; }
             return true;
         } // checking if value is valid according to the rules +
-        public async void Error()
+        public void Error()
         {
            /* 
               owo wats this
@@ -138,43 +199,49 @@ namespace BotCode
               hewe
            */
         } // red blink of the screen
-        public void Ending(bool arg)
-        {
-            /* 
-              owo wats this
-              you not suppose to be
-              hewe
-           */
-        } // win/lose condition
         public void Reset()
         {
-            Region_Array = null;
-            Vertical_Array = null;
-            Horizontal_Array = null;
-            Elements = null;
-            Elements_Prediction = null;
+            Difficulty = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Region_Array[i, j] = "";
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                Horizontal_Array[i] = "";
+                Vertical_Array[i] = "";
+                for(int j = 0; j < 9; j++)
+                {
+                    Elements[i, j] = 0;
+                    Elements_Prediction[i, j] = " ";
+                }
+            }
         } // nullify all arrays +
-        public void Prediction()
+        public int Prediction()
         {
             int Counter_w = 0;
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    if (Elements[i, j] != null)
+                    if (Elements[i, j] != 0)
                     {
                         int Counter_l = 0;
                         for (int a = 1; a < 10; a++)
                         {
-                            if (Is_Valid(i, j, a) == true) { Elements_Prediction[i, j] += Convert.ToString(a); }
+                            if (IsValid(i, j, a) == true) { Elements_Prediction[i, j] += Convert.ToString(a); }
                             else { Counter_l++; }
                         }
-                        if (Counter_l == 9) { Ending(false); }
+                        if (Counter_l == 9) { return (0); }
                     }
                     else { Counter_w++; }
                 }
             }
-            if (Counter_w == 81) { Ending(true); }
+            if (Counter_w == 81) { return (1); }
+            return(-1);
         } // providing possible values in boxes +
         public string Render()
         {
@@ -192,7 +259,7 @@ namespace BotCode
 
             return a;
         } // Returns nice table string
-        public void Enforce_Difficulty(int Difficulty)
+        public void EnforceDifficulty(int Difficulty)
         {
             Random r = new Random();
             int i, j, v;
@@ -201,7 +268,7 @@ namespace BotCode
                 i = r.Next(0, 9);
                 j = r.Next(0, 9);
                 v = r.Next(1, 9);
-                if (Is_Valid(i, j, v) == true)
+                if (IsValid(i, j, v) == true)
                 {
                     Elements[i, j] = v; Difficulty--;
                     Region_Array[i / 3, j / 3] += v; Horizontal_Array[i] += v; Vertical_Array[j] += v;
